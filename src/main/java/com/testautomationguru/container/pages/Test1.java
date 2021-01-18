15fa05d99872bc06108b850b1c302b2d714565a7
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-01-18 22:28:32 +0530

    Update Test1.java

--- src/main/java/com/testautomationguru/container/pages/Test1.java
+++ src/main/java/com/testautomationguru/container/pages/Test1.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SearchPage {
+public class Test1 {
 
     private final WebDriver driver;
     private final WebDriverWait wait;
